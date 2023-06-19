ccbcbc4fdbc130505dfd303a4f7d624a4324088d
Богдан Амяга <bogdan.amyaga2303@gmail.com>
2023-06-20 00:20:23 +0300

    feat: add routing + code splitting +  Lazy Suspence

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import React from 'react'
-import Counter from './components/Counter'
 import './index.scss';
+import { Route, Routes } from 'react-router-dom';
+import {AboutPageAsync} from './pages/AboutPage/AboutPage.async';
+import { MainPageAsync } from './pages/MainPage/MainPage.async';
+import { Link } from 'react-router-dom';
+import { Suspense } from 'react';
+import Spinner from './components/Spinner/Spinner';
 
 const App = () => {
   return (
     <div className='app'>
-        kkfkfkfk
-        <Counter/>
+        <Link to={'/'}>Главная</Link>
+        <Link to={'/about'}>О сайте</Link>
+        <Suspense fallback={<Spinner />}>
+            <Routes>
+                <Route path={'/about'} element={<AboutPageAsync/>}/>
+                <Route path={'/'} element={<MainPageAsync/>}/>
+            </Routes>
+        </Suspense>
     </div>
   )
 }
---
+++ src/components/Spinner/Spinner.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import className from './Spinner.module.scss'
+
+const Spinner = () => {
+  return (
+    <section className={className.loader}></section>
+  )
+}
+
+export default Spinner
