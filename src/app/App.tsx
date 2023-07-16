28d07a204001e449bd4940f4ca297c32aeaac643
Богдан Амяга <bogdan.amyaga2303@gmail.com>
2023-07-16 23:33:49 +0300

    feat: add navbar ui + router

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,9 @@
 import './styles/index.scss';
-import { Route, Routes } from 'react-router-dom';
-import {AboutPage} from 'pages/AboutPage';
-import { MainPage } from 'pages/MainPage';
 import { Link } from 'react-router-dom';
-import { Suspense } from 'react';
-import Spinner from 'components/Spinner/Spinner';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { classNames } from 'shared/lib/classNames/classNames';
+import { AppRouter } from './providers/router';
+import { Navbar } from 'widgets/Navbar';
 
 
 const App = () => {
@@ -15,15 +12,10 @@ const App = () => {
 
   return (
     <div className={classNames('app', {}, [theme])}>
+      <Navbar />
       <button onClick={toggleTheme}>toggle</button>
-        <Link to={'/'}>Главная</Link>
-        <Link to={'/about'}>О сайте</Link>
-        <Suspense fallback={<Spinner />}>
-            <Routes>
-                <Route path={'/about'} element={<AboutPage/>}/>
-                <Route path={'/'} element={<MainPage/>}/>
-            </Routes>
-        </Suspense>
+       
+        <AppRouter />
     </div>
   )
 }
---
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -0,0 +1,23 @@
+import Spinner from 'components/Spinner/Spinner'
+import { Suspense } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { routeConfig } from 'shared/config/routeConfig/routeConfig'
+
+const AppRouter = () => {
+  return (
+    <Suspense fallback={<Spinner />}>
+        <Routes>
+            {
+                Object.values(routeConfig).map(({element, path}) => (
+                    <Route 
+                        key={path} 
+                        element={element} 
+                        path={path} />
+                ))
+            }
+        </Routes>
+    </Suspense>
+  )
+}
+
+export default AppRouter
