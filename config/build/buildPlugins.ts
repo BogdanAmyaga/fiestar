3b6a1ec687e931e046c93b16f04c465a6d78bde7
Богдан Амяга <bogdan.amyaga2303@gmail.com>
2023-07-18 23:51:46 +0300

    fix: add hot module webpack

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -18,6 +18,7 @@ export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPlugi
         //  плагин позволяет использовать переменные
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev)
-        })
+        }),
+        new webpack.HotModuleReplacementPlugin()
     ]
 }
